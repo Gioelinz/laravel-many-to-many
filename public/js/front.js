8c2b139779ed4a40f207a0c6e78c01e17dd0d732
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-30 15:21:53 +0200

    first step all files

--- public/js/front.js
+++ public/js/front.js
@@ -12901,7 +12901,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Laravel\laravel-one-to-many\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Laravel\laravel-many-to-many\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
